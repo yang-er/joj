83a2339aded15296583dd7c5b493ec292e43e00d
小羊 <tlylz99@live.cn>
2018-05-11 18:19:52 +0800

    [A]Compile Module

---
+++ JudgeCore/Compile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JudgeCore
+{
+    interface ICompiler
+    {
+        string IncludePath { get; }
+        string Options { get; }
+        string LibraryDirs { get; }
+        string MasterPath { get; }
+        string ExtraPath { get; }
+        bool Compile(string ans, Guid identify);
+    }
+}
---
+++ JudgeCore/Compiler/msvc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JudgeCore.Compiler
+{
+    public class Msvc : ICompiler
+    {
+        private string VCVersion => "14.13.26128";
+        private string WinVersion => "10.0.16299.0";
+        private string VS2017dir => @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community";
+        private string WinKit => @"C:\Program Files (x86)\Windows Kits\10\bin\{winv}";
+        public string IncludePath => VS2017dir + @"\VC\Tools\MSVC\{vcv}\include;" + @"C:\Program Files (x86)\Windows Kits\10\Include\10.0.16299.0\ucrt;C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64;";
+        public string Options => @"/DDEBUG;";
+        public string LibraryDirs => MasterPath + @"\lib\x64;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.16299.0\ucrt\x64;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.16299.0\um\x64;";
+        public string MasterPath => VS2017dir + @"\VC\Tools\MSVC\{vcv}";
+        public string ExtraPath => VS2017dir + @"\Common7\IDE;C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\{vcv}\bin\Hostx64\x64;C:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64;";
+
+        public bool Compile(string ans, Guid identify)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
