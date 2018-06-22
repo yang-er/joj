86314a4668d576dcaec91229cdb35de8e04a6a32
小羊 <670171961@qq.com>
2018-06-22 21:03:15 +0800

    [A]测试数据

---
+++ data/test_case/output_limit_295K.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main()
+{
+	for(int i = 0; i < 30208; i++)
+		printf("1 + 1 = 2\n");
+	return 0;
+}
---
+++ data/test_case/runtime_divided_by_zero.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+	int a = 0;
+	a = a / a;
+	return 0;
+}
